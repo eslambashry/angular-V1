4a625b0db5105afdab3aae92b39bdbe38b010741
eslam <eslamhussin600@gmail.com>
2024-08-03 20:02:24 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
+import { NavBarComponent } from './Components/nav-bar/nav-bar.component';
+import { HomeComponent } from './Components/home/home.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NotFoundComponent,NavBarComponent,HomeComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Components/home/home.component';
+import { NotFoundComponent } from './Components/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'',component:HomeComponent},
+    {path:'**',component:NotFoundComponent}
+];
